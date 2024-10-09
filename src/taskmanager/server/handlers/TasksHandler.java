8a1d5ab7425f0488a85afa1923340c7846bad24d
IT-Egor <egor.belousov.04@yandex.ru>
2024-10-09 20:59:45 +0300

    Добавить создание и добавление задач через POST запрос

--- src/taskmanager/server/handlers/TasksHandler.java
+++ src/taskmanager/server/handlers/TasksHandler.java
@@ -1,10 +1,10 @@
 package taskmanager.server.handlers;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import taskmanager.exceptions.TaskValidationException;
 import taskmanager.server.typeadapters.DurationAdapter;
 import taskmanager.server.typeadapters.LocalDateTimeAdapter;
 import taskmanager.servise.TaskManager;
@@ -13,6 +13,7 @@ import taskmanager.tasktypes.Task;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -34,42 +35,93 @@ public class TasksHandler implements HttpHandler {
         URI requestURI = exchange.getRequestURI();
         String path = requestURI.getPath();
         String requestMethod = exchange.getRequestMethod();
-        String response;
-        int statusCode;
 
         switch (requestMethod) {
             case "GET":
-                String[] pathElements = path.split("/");
-                if (pathElements.length == 2) {
-                    response = gson.toJson(taskManager.getTasks());
-                    statusCode = 200;
-                } else if (pathElements.length == 3) {
-                    try {
-                        Task task = taskManager.findTask(Integer.parseInt(pathElements[2]));
-                        if (task == null) {
-                            statusCode = 404;
-                            response = String.format("Task with id=%s not found", pathElements[2]);
-                        } else {
-                            response = gson.toJson(task);
-                            statusCode = 200;
-                        }
-                    } catch (NumberFormatException e) {
-                        statusCode = 415;
-                        response = "Task id should be integer";
-                    }
-                } else {
-                    statusCode = 400;
-                    response = "Invalid request";
-                }
+                handleGetRequest(exchange, path);
+                break;
+
+            case "POST":
+                handlePostRequest(exchange);
                 break;
 
             default:
-                statusCode = 405;
-                response = "Method not allowed";
+                int statusCode = 405;
+                String response = "Method not allowed";
+                sendResponse(exchange, statusCode, response);
+        }
+    }
 
+    private void handleGetRequest(HttpExchange exchange, String path) throws IOException {
+        String response;
+        int statusCode;
+        String[] pathElements = path.split("/");
+
+        if (pathElements.length == 2) {
+            response = gson.toJson(taskManager.getTasks());
+            statusCode = 200;
+        } else if (pathElements.length == 3) {
+            try {
+                Task task = taskManager.findTask(Integer.parseInt(pathElements[2]));
+                if (task == null) {
+                    statusCode = 404;
+                    response = String.format("Task with id=%s not found", pathElements[2]);
+                } else {
+                    response = gson.toJson(task);
+                    statusCode = 200;
+                }
+            } catch (NumberFormatException e) {
+                statusCode = 415;
+                response = "Task id should be integer";
+            }
+        } else {
+            statusCode = 400;
+            response = "Invalid request";
         }
         sendResponse(exchange, statusCode, response);
+    }
 
+    private void handlePostRequest(HttpExchange exchange) throws IOException {
+        String response;
+        int statusCode;
+        try {
+            String body = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+            JsonElement jsonElement = JsonParser.parseString(body);
+            if (jsonElement.isJsonObject()) {
+                Task task = gson.fromJson(jsonElement, Task.class);
+                System.out.println("task = " + task);
+                try {
+                    if (task.getId() == 0) {
+                        taskManager.addTask(task);
+                        statusCode = 201;
+                        response = "Task added";
+                    } else {
+                        int updateTaskCode = taskManager.updateTask(task);
+                        if (updateTaskCode > 0) {
+                            statusCode = 201;
+                            response = "Task updated";
+                        } else if (updateTaskCode == -1) {
+                            statusCode = 404;
+                            response = String.format("Task with id=%s not found", task.getId());
+                        } else {
+                            statusCode = 406;
+                            response = "Invalid task";
+                        }
+                    }
+                } catch (TaskValidationException e) {
+                    response = e.getMessage();
+                    statusCode = 406;
+                }
+
+            } else {
+                statusCode = 400;
+                response = "Invalid request";
+            }
+        } catch (JsonSyntaxException e) {
+            statusCode = 400;
+            response = "Invalid request";
+        }
+        sendResponse(exchange, statusCode, response);
     }
 
     private void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
