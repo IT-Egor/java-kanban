b3929eb3659f0e7169ea5c4aa10224b84e8efc8c
IT-Egor <egor.belousov.04@yandex.ru>
2024-10-10 18:23:18 +0300

    Добавить обработку POST запросов для подзадач

--- src/taskmanager/server/handlers/SubtasksHandler.java
+++ src/taskmanager/server/handlers/SubtasksHandler.java
@@ -8,6 +8,7 @@ import taskmanager.exceptions.TaskValidationException;
 import taskmanager.servise.TaskManager;
 import taskmanager.tasktypes.Subtask;
 import taskmanager.tasktypes.Task;
+import taskmanager.utility.Type;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -45,5 +46,60 @@ public class SubtasksHandler extends TasksHandler {
             response = "Invalid request";
         }
         sendResponse(exchange, statusCode, response);
+    }
+
+    @Override
+    protected void handlePostRequest(HttpExchange exchange) throws IOException {
+        String response;
+        int statusCode;
+        try {
+            String body = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+            JsonElement jsonElement = JsonParser.parseString(body);
+            if (jsonElement.isJsonObject()) {
+                Subtask subtask = gson.fromJson(jsonElement, Subtask.class);
+                System.out.println("Subtask = " + subtask);
+                try {
+                    throwIfSubtaskNotValid(subtask);
+                    if (subtask.getId() == 0) {
+                        int subtaskAddStatus = taskManager.addSubtask(subtask);
+                        if (subtaskAddStatus == -1) {
+                            throw new TaskValidationException(
+                                    String.format("Epic with id=%s not found", subtask.getContainingEpicId()));
+                        }
+                        statusCode = 201;
+                        response = "Subtask added";
+                    } else {
+                        int updateSubtaskCode = taskManager.updateTask(subtask);
+                        if (updateSubtaskCode > 0) {
+                            statusCode = 201;
+                            response = "Subtask updated";
+                        } else if (updateSubtaskCode == -1) {
+                            statusCode = 404;
+                            response = String.format("Subtask with id=%s not found", subtask.getId());
+                        } else {
+                            statusCode = 406;
+                            response = "Invalid subtask";
+                        }
+                    }
+                } catch (TaskValidationException e) {
+                    response = e.getMessage();
+                    statusCode = 406;
+                }
+
+            } else {
+                statusCode = 400;
+                response = "Invalid request";
+            }
+        } catch (JsonSyntaxException e) {
+            statusCode = 400;
+            response = "Invalid request";
+        }
+        sendResponse(exchange, statusCode, response);
+    }
+
+    private void throwIfSubtaskNotValid(Subtask subtask) {
+        if (subtask.getType() != Type.SUBTASK || subtask.getStatus() == null || subtask.getContainingEpicId() == 0) {
+            throw new TaskValidationException("Invalid subtask");
+        }
     }
 }
